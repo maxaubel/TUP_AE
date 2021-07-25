3d5f7286cff5ad26746b253ca585ba16eb8193e3
Max Aubel <maximiliano.aubel@sansano.usm.cl>
2021-07-25 11:57:28 -0400

    changes in script

--- alert.py
+++ alert.py
@@ -4,22 +4,15 @@ import sys
 
 webhook_url = "https://discord.com/api/webhooks/868875220121944125/jOBMe52vA4Huo6rbOfMMjnXin_DrSTgq9rLnqPHA_HNEh-1AjhrzYmeLfYhZ-JZ94DNL"
 
-
-
 def send_to_discord(message):
     webhook = DiscordWebhook(url=webhook_url, 
     username="TUP_AE",
     content = message)
-
     response = webhook.execute()
 
 def main():
 	text = " ".join(sys.argv[1:])
-
-
-	send_to_discord(f"Terminó ejecución de \"{text}\" a las {datetime.now()}.")
-
-
+	send_to_discord(f"\"{text}\" a las {datetime.now()}.")
 
 if __name__ == '__main__':
 	main()
