88a4e8618b7041fedf5975b45450efd058b8c67f
Max Aubel <maximiliano.aubel@sansano.usm.cl>
2021-08-04 20:33:51 -0400

    added execs 30 teams

--- script_q1.sh
+++ script_q1.sh
@@ -13,7 +13,7 @@ q2=2
 function first {
 	for q1 in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
 	do
-		echo "$nTeams teams and $nIter generations, with q1: $q1, q2: $q2 (second script)"
+		echo "$nTeams teams and $nIter generations, with q1: $q1, q2: $q2 (q1 iterations)"
 		python alert.py working on $nTeams teams, $nIter, $nIndividuals individuals
 		./TUP instances/umps${nTeams}.txt ${q1} ${q2} ${nIter} ${nIndividuals} > "$foldername"/umps${nTeams}_${q1}_${q2}_${nIter}_${nIndividuals}.txt
 		python alert.py "$foldername"/umps${nTeams}_${q1}_${q2}_${nIter}_${nIndividuals}.txt ./TUP instances/umps${nTeams}.txt ${q1} ${q2} ${nIter} ${nIndividuals} `tail -n 30 "$foldername"/umps${nTeams}_${q1}_${q2}_${nIter}_${nIndividuals}.txt`
